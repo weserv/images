159f7d62a01986b08e890658ed121058bb4f7a61
Kleis Auke Wolthuizen <github@kleisauke.nl>
2015-12-30 13:34:07 +0100

    Load time optimization
    Should fix very long (~6 seconds) loading times. The longest loading
    time on the index is now ~2 seconds (for jpg&w=500&output=gif) and the
    shortest is ~400 ms (for &crop=100,100,400,450) without any caching.

--- src/Api/Api.php
+++ src/Api/Api.php
@@ -6,6 +6,7 @@ use AndriesLouw\imagesweserv\Client;
 use AndriesLouw\imagesweserv\Exception\ImageTooLargeException;
 use AndriesLouw\imagesweserv\Manipulators\ManipulatorInterface;
 use GuzzleHttp\Exception\RequestException;
+use Imagick;
 use ImagickException;
 use Intervention\Image\Exception\InvalidArgumentException;
 use Intervention\Image\Exception\NotReadableException;
@@ -127,6 +128,54 @@ class Api implements ApiInterface
 
         try {
             $image = $this->imageManager->make($tmpFileName);
+
+            // Upscale ImageMagick memory limits for 16GB-ram servers (TODO Should we do this in the policy.xml file?)
+            if ($image->getDriver()->getDriverName() == 'Imagick') {
+                // Get ImageMagick core
+                $imagick = $image->getCore();
+
+                // Set the h*w pixel limit that can exist in memory
+                $imagick->setResourceLimit(imagick::RESOURCETYPE_AREA, 4e+6);
+                // Set maximum disk usage (-1 = infinite)
+                $imagick->setResourceLimit(imagick::RESOURCETYPE_DISK, -1);
+                // Set maximum number of cache files that can be open at once
+                $imagick->setResourceLimit(imagick::RESOURCETYPE_FILE, 768);
+                // Set maximum memory map (in bytes) until things are offloaded to disk
+                $imagick->setResourceLimit(imagick::RESOURCETYPE_MAP, 256e+6);
+                // How much memory to allocate (in bytes)
+                $imagick->setResourceLimit(imagick::RESOURCETYPE_MEMORY, 256e+6);
+                // Use 2 threads (because we have OpenMP enabled) equivalent of Imagick::setResourceLimit(imagick::RESOURCETYPE_THREAD, 2) or MAGICK_THREAD_LIMIT=2;
+                $imagick->setResourceLimit(6, 2);
+                // Set max image width of 4000
+                $imagick->setResourceLimit(9, 4000);
+                // Set max image height of 4000
+                $imagick->setResourceLimit(10, 4000);
+
+                // Display resource values (debugging)
+                if (true == false) {
+                    print("Undefined: ");
+                    print($imagick->getResourceLimit(imagick::RESOURCETYPE_UNDEFINED));
+
+                    print("<br><br>Area: ");
+                    print($imagick->getResourceLimit(imagick::RESOURCETYPE_AREA));
+
+                    print("<br><br>Disk: ");
+                    print($imagick->getResourceLimit(imagick::RESOURCETYPE_DISK));
+
+                    print("<br><br>File: ");
+                    print($imagick->getResourceLimit(imagick::RESOURCETYPE_FILE));
+
+                    print("<br><br>Map: ");
+                    print($imagick->getResourceLimit(imagick::RESOURCETYPE_MAP));
+
+                    print("<br><br>Memory: ");
+                    print($imagick->getResourceLimit(imagick::RESOURCETYPE_MEMORY));
+
+                    print("<br><br>Thread(s): ");
+                    print($imagick->getResourceLimit(6));
+                    die;
+                }
+            }
         } catch (NotReadableException $e) {
             //echo file_get_contents($tmpFileName);
             @unlink($tmpFileName);
