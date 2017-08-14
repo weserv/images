da73c6ab9c4b0315840129f32414feab8d919676
Kleis Auke Wolthuizen <github@kleisauke.nl>
2017-08-14 15:38:58 +0200

    Finish BlurTest

--- tests/Manipulators/BlurTest.php
+++ tests/Manipulators/BlurTest.php
@@ -2,22 +2,160 @@
 
 namespace AndriesLouw\imagesweserv\Test\Manipulators;
 
+use AndriesLouw\imagesweserv\Api\Api;
+use AndriesLouw\imagesweserv\Client;
 use AndriesLouw\imagesweserv\Manipulators\Blur;
 use AndriesLouw\imagesweserv\Test\ImagesweservTestCase;
 use Jcupitt\Vips\Image;
+use Mockery\MockInterface;
 
 class BlurTest extends ImagesweservTestCase
 {
+    /**
+     * @var Client|MockInterface $client
+     */
+    private $client;
+
+    /**
+     * @var Api $api
+     */
+    private $api;
+
+    /**
+     * @var Blur $manipulator
+     */
     private $manipulator;
 
     public function setUp()
     {
+        $this->client = $this->getMockery(Client::class);
+        $this->api = new Api($this->client, $this->getManipulators());
         $this->manipulator = new Blur();
     }
 
     public function testCreateInstance()
     {
         $this->assertInstanceOf(Blur::class, $this->manipulator);
+    }
+
+    public function testBlurRadius1()
+    {
+        $testImage = $this->inputJpg;
+        $expectedImage = $this->expectedDir . '/blur-1.jpg';
+        $params = [
+            'w' => '320',
+            'h' => '240',
+            't' => 'square',
+            'blur' => '1'
+        ];
+
+        $uri = basename($testImage);
+
+        $this->client->shouldReceive('get')->with($uri)->andReturn($testImage);
+
+        /** @var Image $image */
+        $image = $this->api->run($uri, $params);
+
+        $this->assertEquals('jpegload', $image->get('vips-loader'));
+        $this->assertEquals(320, $image->width);
+        $this->assertEquals(240, $image->height);
+        $this->assertSimilarImage($expectedImage, $image);
+    }
+
+    public function testBlurRadius10()
+    {
+        $testImage = $this->inputJpg;
+        $expectedImage = $this->expectedDir . '/blur-10.jpg';
+        $params = [
+            'w' => '320',
+            'h' => '240',
+            't' => 'square',
+            'blur' => '10'
+        ];
+
+        $uri = basename($testImage);
+
+        $this->client->shouldReceive('get')->with($uri)->andReturn($testImage);
+
+        /** @var Image $image */
+        $image = $this->api->run($uri, $params);
+
+        $this->assertEquals('jpegload', $image->get('vips-loader'));
+        $this->assertEquals(320, $image->width);
+        $this->assertEquals(240, $image->height);
+        $this->assertSimilarImage($expectedImage, $image);
+    }
+
+    public function testBlurRadius03()
+    {
+        $testImage = $this->inputJpg;
+        $expectedImage = $this->expectedDir . '/blur-0.3.jpg';
+        $params = [
+            'w' => '320',
+            'h' => '240',
+            't' => 'square',
+            'blur' => '0.3'
+        ];
+
+        $uri = basename($testImage);
+
+        $this->client->shouldReceive('get')->with($uri)->andReturn($testImage);
+
+        /** @var Image $image */
+        $image = $this->api->run($uri, $params);
+
+        $this->assertEquals('jpegload', $image->get('vips-loader'));
+        $this->assertEquals(320, $image->width);
+        $this->assertEquals(240, $image->height);
+        $this->assertSimilarImage($expectedImage, $image);
+    }
+
+    public function testMildBlur()
+    {
+        $testImage = $this->inputJpg;
+        $expectedImage = $this->expectedDir . '/blur-mild.jpg';
+        $params = [
+            'w' => '320',
+            'h' => '240',
+            't' => 'square',
+            'blur' => 'true'
+        ];
+
+        $uri = basename($testImage);
+
+        $this->client->shouldReceive('get')->with($uri)->andReturn($testImage);
+
+        /** @var Image $image */
+        $image = $this->api->run($uri, $params);
+
+        $this->assertEquals('jpegload', $image->get('vips-loader'));
+        $this->assertEquals(320, $image->width);
+        $this->assertEquals(240, $image->height);
+        $this->assertSimilarImage($expectedImage, $image);
+    }
+
+    public function testBlurPngTransparent()
+    {
+        $testImage = $this->inputPngOverlayLayer1;
+        $expectedImage = $this->expectedDir . '/blur-trans.png';
+        $params = [
+            'w' => '320',
+            'h' => '240',
+            't' => 'square',
+            'blur' => '10'
+        ];
+
+        $uri = basename($testImage);
+
+        $this->client->shouldReceive('get')->with($uri)->andReturn($testImage);
+
+        /** @var Image $image */
+        $image = $this->api->run($uri, $params);
+
+        $this->assertEquals('pngload', $image->get('vips-loader'));
+        $this->assertEquals(320, $image->width);
+        $this->assertEquals(240, $image->height);
+        $this->assertSimilarImage($expectedImage, $image);
     }
 
     public function testGetBlur()
