6769a88f2bad9831330a287fe52b4f52e05826e3
Kleis Auke Wolthuizen <github@kleisauke.nl>
2017-11-18 10:43:24 +0100

    Switch to libvips' composite operation

--- src/Manipulators/Background.php
+++ src/Manipulators/Background.php
@@ -65,7 +65,11 @@ class Background extends BaseManipulator
                 $this->isPremultiplied = true;
             }
 
-            $image = $this->composite($image, $backgroundImage);
+            /*
+             * Alpha composite src over dst
+             * Assumes alpha channels are already premultiplied and will be unpremultiplied after
+             */
+            $image = Image::composite([$backgroundImage, $image], [2/*VipsBlendMode::OVER*/], ['premultiplied' => true]);
         } else {
             // If it's a 8bit-alpha channel image or the requested background color hasn't an alpha channel;
             // then flatten the alpha out of an image, replacing it with a constant background color.
@@ -98,56 +102,5 @@ class Background extends BaseManipulator
 
 
         return $image;
-    }
-
-    /**
-     * Alpha composite src over dst
-     * Assumes alpha channels are already premultiplied and will be unpremultiplied after.
-     *
-     * @param Image $src The source image.
-     * @param Image $dst The distance image (in this case the background).
-     *
-     * @return Image The manipulated image.
-     */
-    public function composite(Image $src, Image $dst): Image
-    {
-        // Split src into non-alpha and alpha channels
-        $srcWithoutAlpha = $src->extract_band(0, ['n' => $src->bands - 1]);
-        $srcAlpha = $src->extract_band($src->bands - 1, ['n' => 1])->multiply(1.0 / 255.0);
-
-        // Split dst into non-alpha and alpha channels
-        $dstWithoutAlpha = $dst->extract_band(0, ['n' => $dst->bands - 1]);
-        $dstAlpha = $dst->extract_band($dst->bands - 1, ['n' => 1])->multiply(1.0 / 255.0);
-
-        /**
-         * Compute normalized output alpha channel:
-         *
-         * References:
-         * - http://en.wikipedia.org/wiki/Alpha_compositing#Alpha_blending
-         * - https://github.com/jcupitt/ruby-vips/issues/28#issuecomment-9014826
-         *
-         * out_a = src_a + dst_a * (1 - src_a)
-         *                         ^^^^^^^^^^^
-         *                            t0
-         */
-        $t0 = $srcAlpha->linear(-1.0, 1.0);
-        $outAlphaNormalized = $srcAlpha->add($dstAlpha->multiply($t0));
-
-        /**
-         * Compute output RGB channels:
-         *
-         * Wikipedia:
-         * out_rgb = (src_rgb * src_a + dst_rgb * dst_a * (1 - src_a)) / out_a
-         *                                                ^^^^^^^^^^^
-         *                                                    t0
-         *
-         * Omit division by `out_a` since `Compose` is supposed to output a
-         * premultiplied RGBA image as reversal of premultiplication is handled
-         * externally
-         */
-        $outRGBPremultiplied = $srcWithoutAlpha->add($dstWithoutAlpha->multiply($t0));
-
-        // Combine RGB and alpha channel into output image:
-        return $outRGBPremultiplied->bandjoin($outAlphaNormalized->multiply(255.0));
     }
 }
