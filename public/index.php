fa16c2acc0e993a93745f7309babd84f5ef86879
Kleis Auke Wolthuizen <github@kleisauke.nl>
2018-02-05 21:21:47 +0100

    Add Privacy Policy #121

--- public/index.php
+++ public/index.php
@@ -496,7 +496,11 @@ if (!empty($_GET['url'])) {
             </ul>
             <br />
             <div id="footer">
-                <p><a href="https://github.com/andrieslouw/imagesweserv">Source code available on GitHub</a><br /><a href="//getgrav.org">Design inspired by Grav</a></p>
+                <p>
+                    <a href="https://github.com/andrieslouw/imagesweserv">Source code available on GitHub</a><br />
+                    <a href="https://getgrav.org/">Design inspired by Grav</a><br />
+                    <a href="https://github.com/andrieslouw/imagesweserv/blob/3.x/Privacy-Policy.md">Privacy Policy</a>
+                </p>
             </div>
         </div>
     </nav>
@@ -507,7 +511,7 @@ if (!empty($_GET['url'])) {
                 <li>We don't support animated images (yet), but we do support GIF, JPEG, PNG, BMP, XBM, WebP and other filetypes, even transparent images.</li>
                 <li>We do support IPv6, <a href="http://ipv6-test.com/validate.php?url=$url" rel="nofollow">serving dual stack</a>, and supporting <a href="https://$url/?url=ipv6.google.com/logos/logo.gif">IPv6-only origin hosts</a>.</li>
                 <li>For secure connections over TLS/SSL, you can use <a href="https://$url/"><b>https</b>://$url/</a>. <br /><small class="sslnote">This can be very useful for embedding HTTP images on HTTPS websites. HTTPS origin hosts can be used by <a href="https://github.com/andrieslouw/imagesweserv/issues/33">prefixing the hostname with ssl:</a></small></li>
-                <li>We're part of the <a href="https://www.cloudflare.com/">Cloudflare</a> community. Images are being cached and delivered straight from <a href="https://www.cloudflare.com/network">100+ global datacenters</a>. This ensures the fastest load times and best performance.</li>
+                <li>We're part of the <a href="https://www.cloudflare.com/">Cloudflare</a> community. Images are being cached and delivered straight from <a href="https://www.cloudflare.com/network/">100+ global datacenters</a>. This ensures the fastest load times and best performance.</li>
                 <li>On average, we resize 1 million (10<sup>6</sup>) images per hour, which generates around 25TB of outbound traffic per month.</li>
             </ul>
             <p>Requesting an image:</p>
