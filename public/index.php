214d0b0acaf9cc0ceb39c2f077a92234add7a4c5
Andries Louw Wolthuizen <postmaster@weserv.nl>
2018-03-18 13:27:17 +0100

    New statistics

--- public/index.php
+++ public/index.php
@@ -512,7 +512,7 @@ if (!empty($_GET['url'])) {
                 <li>We do support IPv6, <a href="http://ipv6-test.com/validate.php?url=$url" rel="nofollow">serving dual stack</a>, and supporting <a href="https://$url/?url=ipv6.google.com/logos/logo.gif">IPv6-only origin hosts</a>.</li>
                 <li>For secure connections over TLS/SSL, you can use <a href="https://$url/"><b>https</b>://$url/</a>. <br /><small class="sslnote">This can be very useful for embedding HTTP images on HTTPS websites. HTTPS origin hosts can be used by <a href="https://github.com/andrieslouw/imagesweserv/issues/33">prefixing the hostname with ssl:</a></small></li>
                 <li>We're part of the <a href="https://www.cloudflare.com/">Cloudflare</a> community. Images are being cached and delivered straight from <a href="https://www.cloudflare.com/network/">100+ global datacenters</a>. This ensures the fastest load times and best performance.</li>
-                <li>On average, we resize 1 million (10<sup>6</sup>) images per hour, which generates around 25TB of outbound traffic per month.</li>
+                <li>On average, we resize 2 million (2&times;10<sup>6</sup>) images per hour, which generates around 60TB of outbound traffic per month.</li>
             </ul>
             <p>Requesting an image:</p>
             <ul>
