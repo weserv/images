a550d64bc864984b68fd93a8ba5beb012bd65c68
Andries Louw Wolthuizen <postmaster@weserv.nl>
2018-05-11 11:34:01 +0200

    Updated statistics (bandwidth, datacenters)

--- public/index.php
+++ public/index.php
@@ -511,8 +511,8 @@ if (!empty($_GET['url']) && \is_string($_GET['url'])) {
                 <li>We don't support animated images (yet), but we do support GIF, JPEG, PNG, BMP, XBM, WebP and other filetypes, even transparent images.</li>
                 <li>We do support IPv6, <a href="http://ipv6-test.com/validate.php?url=$url" rel="nofollow">serving dual stack</a>, and supporting <a href="https://$url/?url=ipv6.google.com/logos/logo.gif">IPv6-only origin hosts</a>.</li>
                 <li>For secure connections over TLS/SSL, you can use <a href="https://$url/"><b>https</b>://$url/</a>. <br /><small class="sslnote">This can be very useful for embedding HTTP images on HTTPS websites. HTTPS origin hosts can be used by <a href="https://github.com/andrieslouw/imagesweserv/issues/33">prefixing the hostname with ssl:</a></small></li>
-                <li>We're part of the <a href="https://www.cloudflare.com/">Cloudflare</a> community. Images are being cached and delivered straight from <a href="https://www.cloudflare.com/network/">100+ global datacenters</a>. This ensures the fastest load times and best performance.</li>
-                <li>On average, we resize 2 million (2&times;10<sup>6</sup>) images per hour, which generates around 60TB of outbound traffic per month.</li>
+                <li>We're part of the <a href="https://www.cloudflare.com/">Cloudflare</a> community. Images are being cached and delivered straight from <a href="https://www.cloudflare.com/network/">150+ global datacenters</a>. This ensures the fastest load times and best performance.</li>
+                <li>On average, we resize 2 million (2&times;10<sup>6</sup>) images per hour, which generates around 100TB of outbound traffic per month.</li>
             </ul>
             <p>Requesting an image:</p>
             <ul>
