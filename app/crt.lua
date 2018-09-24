ab8e2bfd9725b77de152c197fbcbfa0130fa7324
Andries Louw Wolthuizen <postmaster@weserv.nl>
2018-09-24 16:50:16 +0200

    Remove unused declarations from crt.lua (Travis CI)

--- app/crt.lua
+++ app/crt.lua
@@ -1,15 +1,6 @@
 local config = require "config"
 local template = require "resty.template"
-local redis = require "resty.redis"
 local ngx = ngx
-local type = type
-local pairs = pairs
-local string = string
-
-local ip_address = ngx.var.http_x_forwarded_for or ngx.var.remote_addr
-
--- Be careful: keys and values are unescaped according to URI escaping rules.
-local args, args_err = ngx.req.get_uri_args()
 
 ngx.status = ngx.HTTP_OK
 ngx.header["Content-Type"] = "text/html"
