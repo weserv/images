weserv/images

commit f8dbb8870452904df805d6bf1f30da148fc3c56b

Fix &t=square dimension bug
- Use Ubuntu 16.04 on Travis.
- Only log keepalive errors.
- Bump Travis libvips to 8.7.2.

.travis.yml
@@ -2,12 +2,12 @@ sudo: false
 
 language: generic
 
-dist: trusty
+dist: xenial
 
 env:
   global:
-    - LUAROCKS_VERSION=3.0.3
-    - VIPS_VERSION=8.7.0
+    - LUAROCKS_VERSION=3.0.4
+    - VIPS_VERSION=8.7.2
     - VIPS_TAR_VERSION=$VIPS_VERSION
     - VIPS_WARNING=0
     - TEST_COVERAGE=1
@@ -45,6 +45,7 @@ addons:
       - libpango1.0-dev
       - libpoppler-glib-dev
       - librsvg2-dev
+      - libtiff5-dev
       - libwebp-dev
 
 before_install:

src/weserv/manipulators/thumbnail.lua
@@ -146,8 +146,8 @@ function manipulator.process(image, args)
     end
 
     -- Scaling calculations
-    local target_resize_width = args.w
-    local target_resize_height = args.h
+    local thumbnail_width = args.w
+    local thumbnail_height = args.h
 
     local fit = manipulator.resolve_fit(args.t)
 
@@ -158,21 +158,24 @@ function manipulator.process(image, args)
         local y_factor = input_height / args.h
 
         if fit == "square" or fit == "squaredown" or fit == "crop" then
+            -- We aim to fill the bounding box, so we must use centre as crop mode
+            thumbnail_options.crop = "centre"
+
             if x_factor < y_factor then
-                target_resize_height = math_floor((input_height / x_factor) + 0.5)
+                thumbnail_height = math_floor((input_height / x_factor) + 0.5)
             else
-                target_resize_width = math_floor((input_width / y_factor) + 0.5)
+                thumbnail_width = math_floor((input_width / y_factor) + 0.5)
             end
         elseif fit == "letterbox" or fit == "fit" or fit == "fitup" then
             if x_factor > y_factor then
-                target_resize_height = math_floor((input_height / x_factor) + 0.5)
+                thumbnail_height = math_floor((input_height / x_factor) + 0.5)
             else
-                target_resize_width = math_floor((input_width / y_factor) + 0.5)
+                thumbnail_width = math_floor((input_width / y_factor) + 0.5)
             end
         end
     elseif args.w > 0 then -- Fixed width
         if fit == "absolute" then
-            target_resize_height = input_height
+            thumbnail_height = input_height
             args.h = input_height
         else
             -- Auto height
@@ -181,11 +184,11 @@ function manipulator.process(image, args)
 
             -- Height is missing, replace with a huuuge value to prevent
             -- reduction or enlargement in that axis
-            target_resize_height = VIPS_MAX_COORD
+            thumbnail_height = VIPS_MAX_COORD
         end
     elseif args.h > 0 then -- Fixed height
         if fit == "absolute" then
-            target_resize_width = input_width
+            thumbnail_width = input_width
             args.w = input_width
         else
             -- Auto width
@@ -194,14 +197,14 @@ function manipulator.process(image, args)
 
             -- Width is missing, replace with a huuuge value to prevent
             -- reduction or enlargement in that axis
-            target_resize_width = VIPS_MAX_COORD
+            thumbnail_width = VIPS_MAX_COORD
         end
     else
         -- Identity transform
-        target_resize_width = input_width
+        thumbnail_width = input_width
         args.w = input_width
 
-        target_resize_height = input_height
+        thumbnail_height = input_height
         args.h = input_height
 
         -- No need to check max image size because
@@ -211,11 +214,11 @@ function manipulator.process(image, args)
 
     if swap_needed then
         -- Swap target output width and height when rotating by 90 or 270 degrees
-        target_resize_width, target_resize_height = target_resize_height, target_resize_width
+        thumbnail_width, thumbnail_height = thumbnail_height, thumbnail_width
     end
 
     -- Assign settings
-    thumbnail_options.height = target_resize_height
+    thumbnail_options.height = thumbnail_height
 
     if fit == "absolute" then
         thumbnail_options.size = "force"
@@ -229,7 +232,7 @@ function manipulator.process(image, args)
         thumbnail_options.size = "both"
     end
 
-    -- targetResizeWidth and targetResizeWidth aren't reliable anymore.
+    -- thumbnail_width and thumbnail_height aren't reliable anymore.
     if check_max_image_size and args.w * args.h > MAX_IMAGE_SIZE then
         error({
             status = HTTP_NOT_FOUND,
@@ -242,10 +245,10 @@ function manipulator.process(image, args)
     --
     -- Note: After this operation the pixel interpretation is sRGB or RGB
     if args.trim ~= nil or args.gam ~= nil then
-        return image:thumbnail_image(target_resize_width, thumbnail_options)
+        return image:thumbnail_image(thumbnail_width, thumbnail_options)
     else
         return vips.Image.thumbnail(args.tmp_file_name .. args.string_options,
-            target_resize_width, thumbnail_options)
+            thumbnail_width, thumbnail_options)
     end
 end
 

spec/manipulators/thumbnail_spec.lua
@@ -198,6 +198,21 @@ describe("thumbnail manipulator", function()
             assert.False(utils.has_alpha(image))
         end)
 
+        -- Use the smaller axis in crop mode, we aim to fill the bounding box
+        it("smaller axis", function()
+            local params = {
+                w = "123",
+                h = "100",
+                t = "square"
+            }
+
+            local image = api:process(test_image, params)
+
+            assert.equal(123, image:width())
+            assert.equal(100, image:height())
+            assert.False(utils.has_alpha(image))
+        end)
+
         it("upscale", function()
             local params = {
                 w = "3000",

.ci/install-luarocks.sh
@@ -6,12 +6,12 @@ luarocks_tarball=https://luarocks.github.io/luarocks/releases/luarocks-${version
 set -e
 
 # Do we already have the correct LuaRocks built?
-if [ -d "$HOME/luarocks/bin" ]; then
+if [[ -d "$HOME/luarocks/bin" ]]; then
     installed_version=$($HOME/luarocks/bin/luarocks --version | head -1 | awk '{print $2}')
     echo "Need LuaRocks $version"
     echo "Found LuaRocks $installed_version"
 
-    if [ "$installed_version" == "$version" ]; then
+    if [[ "$installed_version" == "$version" ]]; then
         echo "Using cached LuaRocks directory"
         exit 0
     fi

.ci/install-vips.sh
@@ -8,7 +8,7 @@ vips_tarball=https://github.com/libvips/libvips/releases/download/v${version}${p
 set -e
 
 # Do we already have the correct vips built?
-if [ -d "$HOME/vips/bin" ]; then
+if [[ -d "$HOME/vips/bin" ]]; then
     installed_version=$($HOME/vips/bin/vips --version | awk -F- '{print $2}')
     echo "Need vips $version"
     echo "Found vips $installed_version"

spec/client_spec.lua
@@ -266,18 +266,14 @@ describe("client", function()
         it("keepalive error", function()
             errors.keepalive = "timeout"
 
-            local res, err = client:request("https://ory.weserv.nl/lichtenstein.jpg?foo=bar")
-
-            assert.equal(404, err.status)
-            assert.equal("Failed to set keepalive.", err.message)
-            assert.falsy(res)
+            local _, _ = client:request("https://ory.weserv.nl/lichtenstein.jpg?foo=bar")
 
             assert.spy(stubbed_http.connect).was.called()
             assert.spy(stubbed_http.ssl_handshake).was.called()
             assert.spy(stubbed_http.request).was.called()
             assert.spy(stubbed_http.set_keepalive).was.called()
 
-            -- Log keepalive errors
+            -- Only log keepalive errors
             assert.equal(1, #ngx._logs)
         end)
 

src/weserv/client.lua
@@ -255,13 +255,8 @@ function client:request(uri, addl_headers, redirect_nr)
 
     local ok, keepalive_err = httpc:set_keepalive()
     if not ok then
+        -- Only log keepalive errors, don't bother the user with internal errors.
         ngx_log(ngx_ERR, "Failed to set keepalive: ", keepalive_err)
-        os_remove(res.tmpfile)
-
-        return nil, {
-            status = HTTP_NOT_FOUND,
-            message = "Failed to set keepalive.",
-        }
     end
 
     return res, nil

commit 73c3f4b5e161eac9d2c90550a3d489f586504305

Fix &t=squaredown oversample bug (#178)
Regression from: f8dbb8870452904df805d6bf1f30da148fc3c56b.

.travis.yml
@@ -1,13 +1,12 @@
-sudo: false
-
 language: generic
 
+# Ubuntu 16.04
 dist: xenial
 
 env:
   global:
     - LUAROCKS_VERSION=3.0.4
-    - VIPS_VERSION=8.7.2
+    - VIPS_VERSION=8.7.4
     - VIPS_TAR_VERSION=$VIPS_VERSION
     - VIPS_WARNING=0
     - TEST_COVERAGE=1
@@ -31,22 +30,25 @@ addons:
       - openresty-resty
       - libexpat1-dev
       - gettext
-      - libglib2.0-dev
-      - liborc-0.4-dev
-      - libfftw3-dev
       - liblcms2-dev
       - libmagickwand-dev
       - libopenexr-dev
       - libcfitsio3-dev
+      - libfftw3-dev
       - libgif-dev
+      - libgs-dev
       - libgsf-1-dev
       - libmatio-dev
       - libopenslide-dev
+      - liborc-0.4-dev
       - libpango1.0-dev
       - libpoppler-glib-dev
       - librsvg2-dev
-      - libtiff5-dev
       - libwebp-dev
+      - libtiff5-dev
+      # missing on xenial, unfortunately
+      # - libwebpmux2
+      - libglib2.0-dev
 
 before_install:
   - bash .ci/install-vips.sh

src/weserv/manipulators/thumbnail.lua
@@ -158,13 +158,16 @@ function manipulator.process(image, args)
         local y_factor = input_height / args.h
 
         if fit == "square" or fit == "squaredown" or fit == "crop" then
-            -- We aim to fill the bounding box, so we must use centre as crop mode
-            thumbnail_options.crop = "centre"
-
             if x_factor < y_factor then
                 thumbnail_height = math_floor((input_height / x_factor) + 0.5)
             else
                 thumbnail_width = math_floor((input_width / y_factor) + 0.5)
+            end
+
+            -- We aim to fill the bounding box, so we must use centre as crop mode.
+            -- Only if we're not oversampling the image with squaredown.
+            if fit ~= "squaredown" or (thumbnail_width <= input_width and thumbnail_height <= input_height) then
+                thumbnail_options.crop = "centre"
             end
         elseif fit == "letterbox" or fit == "fit" or fit == "fitup" then
             if x_factor > y_factor then

spec/manipulators/thumbnail_spec.lua
@@ -203,13 +203,28 @@ describe("thumbnail manipulator", function()
             local params = {
                 w = "123",
                 h = "100",
-                t = "square"
+                t = "squaredown"
             }
 
             local image = api:process(test_image, params)
 
             assert.equal(123, image:width())
             assert.equal(100, image:height())
+            assert.False(utils.has_alpha(image))
+        end)
+
+        -- Don't use the crop mode when oversampling with squaredown
+        it("smaller axis down width", function()
+            local params = {
+                w = "3000",
+                h = "3000",
+                t = "squaredown"
+            }
+
+            local image = api:process(test_image, params)
+
+            assert.equal(2725, image:width())
+            assert.equal(2225, image:height())
             assert.False(utils.has_alpha(image))
         end)
 