9302dab3c7d7141bad46dc5435320345b65462bf
Kleis Auke Wolthuizen <github@kleisauke.nl>
2019-10-20 19:56:16 +0200

    Disable HEIF shrink-on-load for libvips < 8.9
    See: libvips/libvips@1ef1b2d

--- src/api/processors/thumbnail.cpp
+++ src/api/processors/thumbnail.cpp
@@ -279,6 +279,10 @@ VImage Thumbnail::shrink_on_load(const VImage &image,
 
         return VImage::new_from_buffer(buffer, "",
                                        load_options->set("scale", scale));
+#if VIPS_VERSION_AT_LEAST(8, 9, 0)
+    // Retrieving a non-existent thumbnail from a HEIF image was terribly slow
+    // before libvips 8.9.0. See:
+    // https://github.com/libvips/libvips/commit/1ef1b2d9870d8be9c1a063a47f0c745c04a127d3
     } else if (image_type == ImageType::Heif) {
         append_page_options(load_options);
 
@@ -291,6 +295,7 @@ VImage Thumbnail::shrink_on_load(const VImage &image,
         if (resolve_common_shrink(thumb.width(), thumb.height()) >= 1.0) {
             return thumb;
         }
+#endif
     }
 
     // Still here? Just return the original image
